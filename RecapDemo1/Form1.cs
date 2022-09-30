e0804633d7524d413391b3df22a6337292e2035d
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-09-30 22:22:35 +0300

    Form 1 e degisiklik yapildi

--- RecapDemo1/Form1.cs
+++ RecapDemo1/Form1.cs
@@ -19,6 +19,32 @@ namespace RecapDemo1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int left = 0;
+            int top = 0;
+
+            Button [,] buttons  = new Button[8,8];
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i,j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i,j].Height = 50;
+                    this.Controls.Add(buttons[i, j]);
+                    buttons[i, j].Left = left;
+                    buttons[i,j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else buttons[i, j].BackColor = Color.White;
+
+                }
+                left = 0;
+                top += 50;
+
+            }
 
         }
     }
